ba9ce4bbca687811410a5dc2162c44c1f99cfff9
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-11-30 10:59:13 +0100

    Added more cells.

---
+++ Data/PANASONIC_NCR_18650_A.h
@@ -0,0 +1,12 @@
+// This file contains the information of a battery type cell.
+// It is important to use the same size table for all battery types.
+// First row is the voltage
+// Second row is the percentage in capacity
+// Third row is the percentage in energy
+
+
+uint8_t batteryAh[][] = {
+	{2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5},
+	{  0,   0,   0,   0,   0,   1,  11,  20,  38,  52,  60,  73,  83,  94, 100, 100, 100, 100},
+	{  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0}
+};
--- lithium.h
+++ lithium.h
@@ -1,5 +1,13 @@
 #include ".../lithium.c"
+
+
+// Include the cell type you want to use
+
 #include ".../Data/AW18650.h"
+//#include ".../Data/SANYO18650_2600.h"
+//#include ".../Data/PANASONIC_CGR_18650_CH.h"
+//#include ".../Data/PANASONIC_NCR_18650_A.h"
+//#include ".../Data/PANASONIC_NCR_18650_B.h"
 
 uint8_t GivePercent(float voltage, uint8_t capType);
 // Returns remaining percentage of the battery
